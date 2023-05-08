de487f4d3f4af62f04419561a981a7a55dc02712
Oliver Grüttner <gruettner@protonmail.com>
2023-05-09 01:19:12 +0200

    fix: corrected to german

--- src/components/RightSidebar/TableOfContents.tsx
+++ src/components/RightSidebar/TableOfContents.tsx
@@ -70,7 +70,7 @@ const TableOfContents: FunctionalComponent<{ headings: MarkdownHeading[] }> = ({
 
   return (<>
       <h2 id={onThisPageID} className="heading">
-        On this page
+        Auf dieser Seite
       </h2>
 
       <ul ref={toc as any}>
